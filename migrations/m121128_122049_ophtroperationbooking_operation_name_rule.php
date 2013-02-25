c1e3d662d4033fab6bfbf76a4a9a02d90b68dab8
Mark <m@rkw.me>
2013-02-25 16:08:07 +0000

    [OE-2688] typo

--- migrations/m121128_122049_ophtroperationbooking_operation_name_rule.php
+++ migrations/m121128_122049_ophtroperationbooking_operation_name_rule.php
@@ -17,8 +17,8 @@ class m121128_122049_ophtroperationbooking_operation_name_rule extends CDbMigrat
 				'KEY `ophtroperationbooking_operation_name_r_lmui_fk` (`last_modified_user_id`)',
 				'KEY `ophtroperationbooking_operation_name_r_cid_fk` (`created_user_id`)',
 				'CONSTRAINT `ophtroperationbooking_operation_name_rt_id_fk` FOREIGN KEY (`theatre_id`) REFERENCES `ophtroperationbooking_operation_theatre` (`id`)',
-				'CONSTRAINT `ophtroperationbooking_operation_name_r_lmui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
-				'CONSTRAINT `ophtroperationbooking_operation_name_r_cid_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)'
+				'CONSTRAINT `ophtroperationbooking_operation_name_r_cid_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+				'CONSTRAINT `ophtroperationbooking_operation_name_r_lmui_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)'
 			),
 			'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin'
 		);
