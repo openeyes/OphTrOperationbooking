edc3d3a28972be80db2cc9eeb21b7552a0985ba4
Mark <m@rkw.me>
2013-02-25 16:06:49 +0000

    [OE-2688] fixed broken foreign key

--- migrations/m121128_122049_ophtroperationbooking_operation_name_rule.php
+++ migrations/m121128_122049_ophtroperationbooking_operation_name_rule.php
@@ -13,12 +13,12 @@ class m121128_122049_ophtroperationbooking_operation_name_rule extends CDbMigrat
 				'created_user_id' => 'int(10) unsigned NOT NULL DEFAULT \'1\'',
 				'created_date' => 'datetime NOT NULL DEFAULT \'1900-01-01 00:00:00\'',
 				'PRIMARY KEY (`id`)',
-				'KEY `ophtroperationbooking_operation_name_rule_theatre_id_fk` (`theatre_id`)',
-				'KEY `ophtroperationbooking_operation_name_rule_last_modified_user_id_fk` (`last_modified_user_id`)',
-				'KEY `ophtroperationbooking_operation_name_rule_created_user_id_fk` (`created_user_id`)',
-				'CONSTRAINT `ophtroperationbooking_operation_name_rule_theatre_id_fk` FOREIGN KEY (`theatre_id`) REFERENCES `ophtroperationbooking_operation_theatre` (`id`)',
-				'CONSTRAINT `ophtroperationbooking_operation_name_rule_last_modified_user_id_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
-				'CONSTRAINT `ophtroperationbooking_operation_name_rule_created_user_id_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)'
+				'KEY `ophtroperationbooking_operation_name_rt_id_fk` (`theatre_id`)',
+				'KEY `ophtroperationbooking_operation_name_r_lmui_fk` (`last_modified_user_id`)',
+				'KEY `ophtroperationbooking_operation_name_r_cid_fk` (`created_user_id`)',
+				'CONSTRAINT `ophtroperationbooking_operation_name_rt_id_fk` FOREIGN KEY (`theatre_id`) REFERENCES `ophtroperationbooking_operation_theatre` (`id`)',
+				'CONSTRAINT `ophtroperationbooking_operation_name_r_lmui_fk` FOREIGN KEY (`created_user_id`) REFERENCES `user` (`id`)',
+				'CONSTRAINT `ophtroperationbooking_operation_name_r_cid_fk` FOREIGN KEY (`last_modified_user_id`) REFERENCES `user` (`id`)'
 			),
 			'ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin'
 		);
