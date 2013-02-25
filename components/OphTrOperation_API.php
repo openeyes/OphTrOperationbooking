0c6c4143d6010e9ec84fa89e6bf6940c6b757b3d
Mark <m@rkw.me>
2013-02-25 11:43:26 +0000

    operation completed status

--- components/OphTrOperation_API.php
+++ components/OphTrOperation_API.php
@@ -34,11 +34,66 @@ class OphTrOperation_API {
 			->findAll($criteria);
 	}
 
+	/* Gets 'open' bookings for the specified episode
+	 * A booking is deemed open if it has no operation note linked to it */
+	public function getOpenBookingsForEpisode($episode_id) {
+		$criteria = new CDbCriteria;
+		$criteria->order = 'datetime asc';
+		$criteria->addCondition('episode_id',$episode_id);
+
+		$status_scheduled = OphTrOperation_Operation_Status::model()->find('name=?',array('Scheduled'));
+		$status_rescheduled = OphTrOperation_Operation_Status::model()->find('name=?',array('Rescheduled'));
+
+		return OphTrOperation_Operation_Booking::model()
+			->with('session')
+			->with(array(
+				'operation' => array(
+					'condition' => "episode_id = $episode_id and status_id in ($status_scheduled->id,$status_rescheduled->id)",
+					'with' => 'event'
+				)
+			))
+			->findAll($criteria);
+	}
+
 	public function getOperationProcedures($operation_id) {
 		return OphTrOperation_Operation_Procedures::model()->findAll('element_id=?',array($operation_id));
 	}
 
 	public function getOperationForEvent($event_id) {
 		return Element_OphTrOperation_Operation::model()->find('event_id=?',array($event_id));
+	}
+
+	public function setOperationStatus($event_id, $status_name) {
+		if (!$status = OphTrOperation_Operation_Status::model()->find('name=?',array($status_name))) {
+			throw new Exception("Unknown operation status: $status_name");
+		}
+
+		if (!$operation = Element_OphTrOperation_Operation::model()->find('event_id=?',array($event_id))) {
+			throw new Exception("Operation event not found: $event_id");
+		}
+
+		If ($operation->status_id != $status->id) {
+			$operation->status_id = $status->id;
+
+			if (!$operation->save()) {
+				throw new Exception("Unable to save operation: ".print_r($operation->getErrors(),true));
+			}
+		}
+	}
+
+	public function getProceduresForOperation($event_id) {
+		if (!$operation = Element_OphTrOperation_Operation::model()->find('event_id=?',array($event_id))) {
+			throw new Exception("Operation event not found: $event_id");
+		}
+
+		return $operation->procedures;
+	}
+
+	public function getEyeForOperation($event_id) {
+		if (!$operation = Element_OphTrOperation_Operation::model()->find('event_id=?',array($event_id))) {
+			throw new Exception("Operation event not found: $event_id");
+		}
+
+		return $operation->eye;
 	}
 }
