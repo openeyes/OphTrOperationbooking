74e4f2c38184354aa2837d4fca7f030e5114a0c7
Mark <m@rkw.me>
2013-04-25 10:08:48 +0100

    [OE-2958] fixed print buttons failing to hide when entering edit mode on the theatre diary.

--- assets/js/TheatreDiaryController.js
+++ assets/js/TheatreDiaryController.js
@@ -211,6 +211,7 @@ $(document).ready(function() {
 		$('#tbody_'+theatre_edit_session_id+' .diaryViewMode').hide();
 		$('div.session_options.diaryViewMode').hide();
 		$('div.comments_ro.diaryViewMode').hide();
+		$('.printButtons .diaryViewMode').hide();
 		$('.diaryEditMode[data-id="'+theatre_edit_session_id+'"]').show();
 		$('.action_options[data-id="'+theatre_edit_session_id+'"]').show();
 
