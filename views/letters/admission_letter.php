03ae6a09a563c77c727406696d6d820e93229113
Mark <m@rkw.me>
2013-05-29 15:33:06 +0100

    letter bugfix

--- views/letters/admission_letter.php
+++ views/letters/admission_letter.php
@@ -133,7 +133,7 @@
 
 	<?php if ($patient->isChild()) {?>
 		<p>
-			If there has been any change in your child's general health, such as a cough or cold, any infectious disease, or any other condition which might affect their fitness for operation, please telephone <?php echo $operation->letterContact->refuse_telephone?> and ask to speak to a nurse for advice.
+			If there has been any change in your child's general health, such as a cough or cold, any infectious disease, or any other condition which might affect their fitness for operation, please telephone <?php /* FIXME: this should not be hard coded */?> 020 7566 2595 and ask to speak to a nurse for advice.
 		</p>
 		<p>
 			If you do not speak English, please arrange for an English speaking adult to stay with you until you reach the ward and have been seen by a doctor and anaesthetist.
