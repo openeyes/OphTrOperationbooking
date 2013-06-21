76b2f3deb66fda3baf5a456229ab366bab252bc6
Mark <m@rkw.me>
2013-06-21 14:20:51 +0100

    [OE-3096] infer day of the week from the start date when adding and editing sequences

--- views/admin/editsequence.php
+++ views/admin/editsequence.php
@@ -34,6 +34,13 @@
 			<?php echo $form->dropDownList($sequence,'theatre_id',CHtml::listData(OphTrOperationbooking_Operation_Theatre::model()->findAll(array('order'=>'name')),'id','name'),array('empty'=>'- None -'))?>
 			<?php echo $form->datePicker($sequence,'start_date',array(),array('null'=>true))?>
 			<?php echo $form->datePicker($sequence,'end_date',array(),array('null'=>true))?>
+			<div id="div_OphTrOperationbooking_Operation_Sequence_weekday" class="eventDetail">
+				<div class="label">Weekday:</div>
+				<div class="data">
+					<span id="weekday" class="label" style="margin-top: 6px; margin-bottom: 0;"><?php if ($sequence->id) { echo date('l',strtotime($sequence->start_date)); } else { echo 'Not set'; }?></span>
+					<?php echo $form->hiddenField($sequence,'weekday')?>
+				</div>
+			</div>
 			<?php echo $form->textField($sequence,'start_time',array('size'=>10))?>
 			<?php echo $form->textField($sequence,'end_time',array('size'=>10))?>
 			<?php echo $form->dropDownList($sequence,'interval_id',CHtml::listData(OphTrOperationbooking_Operation_Sequence_Interval::model()->findAll(array('order'=>'name')),'id','name'),array('empty'=>'- None -'))?>
@@ -86,5 +93,24 @@
 	handleButton($('#et_add_session_to_sequence'),function(e) {
 		e.preventDefault();
 		window.location.href = baseUrl+'/OphTrOperationbooking/admin/addSession?sequence_id=<?php echo $sequence->id?>';
+	});
+	$('#OphTrOperationbooking_Operation_Sequence_start_date_0').change(function() {
+		var d = new Date($(this).val());
+		var day_id = d.getDay();
+		if (day_id == 0) {
+			day_id = 7;
+		}
+
+		switch (day_id) {
+			case 1: $('#weekday').text('Monday'); break;
+			case 2: $('#weekday').text('Tuesday'); break;
+			case 3: $('#weekday').text('Wednesday'); break;
+			case 4: $('#weekday').text('Thursday'); break;
+			case 5: $('#weekday').text('Friday'); break;
+			case 6: $('#weekday').text('Saturday'); break;
+			case 7: $('#weekday').text('Sunday'); break;
+		}
+
+		$('#OphTrOperationbooking_Operation_Sequence_weekday').val(day_id);
 	});
 </script>
