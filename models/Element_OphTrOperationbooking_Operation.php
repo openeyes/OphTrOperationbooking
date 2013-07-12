950caa360b55a808dae2d5633f3985984d41e85e
Mark <m@rkw.me>
2013-07-12 11:33:44 +0100

    [OE-3094] disallow editing completed operations.

--- models/Element_OphTrOperationbooking_Operation.php
+++ models/Element_OphTrOperationbooking_Operation.php
@@ -1,49 +1,50 @@
-<?php /**
-	 * OpenEyes
-	 *
-	 * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
-	 * (C) OpenEyes Foundation, 2011-2013
-	 * This file is part of OpenEyes.
-	 * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-	 * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-	 * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
-	 *
-	 * @package OpenEyes
-	 * @link http://www.openeyes.org.uk
-	 * @author OpenEyes <info@openeyes.org.uk>
-	 * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
-	 * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
-	 * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
-	 */
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
 
-	/**
-	 * This is the model class for table "et_ophtroperationbooking_operation".
-	 *
-	 * The followings are the available columns in table:
-	 * @property string $id
-	 * @property integer $event_id
-	 * @property integer $eye_id
-	 * @property integer $consultant_required
-	 * @property integer $anaesthetic_type_id
-	 * @property integer $overnight_stay
-	 * @property integer $site_id
-	 * @property integer $priority_id
-	 * @property string $decision_date
-	 * @property string $comments
-	 *
-	 * The followings are the available model relations:
-	 *
-	 * @property ElementType $element_type
-	 * @property EventType $eventType
-	 * @property Event $event
-	 * @property User $user
-	 * @property User $usermodified
-	 * @property Eye $eye
-	 * @property OphTrOperationbooking_Operation_Procedures $procedures
-	 * @property AnaestheticType $anaesthetic_type
-	 * @property Site $site
-	 * @property Element_OphTrOperationbooking_Operation_Priority $priority
-	 */
+/**
+ * This is the model class for table "et_ophtroperationbooking_operation".
+ *
+ * The followings are the available columns in table:
+ * @property string $id
+ * @property integer $event_id
+ * @property integer $eye_id
+ * @property integer $consultant_required
+ * @property integer $anaesthetic_type_id
+ * @property integer $overnight_stay
+ * @property integer $site_id
+ * @property integer $priority_id
+ * @property string $decision_date
+ * @property string $comments
+ *
+ * The followings are the available model relations:
+ *
+ * @property ElementType $element_type
+ * @property EventType $eventType
+ * @property Event $event
+ * @property User $user
+ * @property User $usermodified
+ * @property Eye $eye
+ * @property OphTrOperationbooking_Operation_Procedures $procedures
+ * @property AnaestheticType $anaesthetic_type
+ * @property Site $site
+ * @property Element_OphTrOperationbooking_Operation_Priority $priority
+ */
 
 	class Element_OphTrOperationbooking_Operation extends BaseEventTypeElement
 	{
@@ -852,7 +853,7 @@
 	}
 
 	public function isEditable() {
-		return $this->status->name != 'Cancelled';
+		return $this->status->name != 'Cancelled' && $this->status->name != 'Completed';
 	}
 
 	public function schedule($booking_attributes, $operation_comments, $session_comments, $reschedule=false) {
