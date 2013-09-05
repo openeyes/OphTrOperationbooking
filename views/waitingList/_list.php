9810e2f3e4872b0baa3e37d3cd581faedabeccaa
splatEric <mike.smith@camc-ltd.co.uk>
2013-09-05 09:53:46 +0100

    + [OE-3303] fix logic for displaying the checkbox for selecting operation for printing gp letter if no practice available. + fixed indentation

--- views/waitingList/_list.php
+++ views/waitingList/_list.php
@@ -96,7 +96,11 @@
 						<td><?php echo ucfirst(preg_replace('/^Requires /','',$eo->status->name)) ?></td>
 						<td<?php if ($tablecolour == 'White' && Yii::app()->user->checkAccess('admin')) { ?> class="admin-td"<?php } ?>>
 
-							<?php if (($patient && $patient->contact->correspondAddress) && $eo->id && ($eo->getDueLetter() != Element_OphTrOperationbooking_Operation::LETTER_GP || ($eo->getDueLetter() == Element_OphTrOperationbooking_Operation::LETTER_GP && $patient->practice_id))) {?>
+							<?php if (($patient && $patient->contact->correspondAddress)
+								&& $eo->id
+								&& ($eo->getDueLetter() != Element_OphTrOperationbooking_Operation::LETTER_GP
+									|| ($eo->getDueLetter() == Element_OphTrOperationbooking_Operation::LETTER_GP && $patient->practice && $patient->practice->contact->address)
+								)) {?>
 							<div>
 								<input<?php if ($tablecolour == 'White' && !Yii::app()->user->checkAccess('admin')) { ?> disabled="disabled"<?php } ?> type="checkbox" id="operation<?php echo $eo->id ?>" value="1" />
 							</div>
