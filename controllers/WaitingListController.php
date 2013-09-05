b81e63af8abc5cfe389fbdf2f432f2e015d846f2
splatEric <mike.smith@camc-ltd.co.uk>
2013-09-05 09:45:22 +0100

    + [OE-3303] fix level access check for print permissions

--- controllers/WaitingListController.php
+++ controllers/WaitingListController.php
@@ -25,7 +25,7 @@ class WaitingListController extends BaseEventTypeController
 			// Level 2 or below can't change anything
 			array('deny',
 				'actions' => array('confirmprinted', 'printletters'),
-				'expression' => '!BaseController::checkUserLevel(4)',
+				'expression' => '!BaseController::checkUserLevel(3)',
 			),
 			// Level 2 or above can do anything else
 			array('allow',
