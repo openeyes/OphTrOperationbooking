88aad9d30dedcd748d0bed20ce67626eb4f883a7
Mike Smith <mike.smith@openeyes.org.uk>
2013-12-11 11:11:22 +0000

    + [OE-2927] minor bugfix

--- controllers/BookingController.php
+++ controllers/BookingController.php
@@ -148,7 +148,7 @@ class BookingController extends BaseEventTypeController
 		$this->processJsVars();
 
 		$this->render('schedule', array(
-			'event' => $event,
+			'event' => $this->event,
 			'operation' => $operation,
 			'firm' => $firm,
 			'firmList' => Firm::model()->listWithSpecialties,
@@ -178,7 +178,7 @@ class BookingController extends BaseEventTypeController
 
 	/**
 	 * Cancels and reschedules an operation
-	 * 
+	 *
 	 * @throws Exception
 	 */
 	public function actionRescheduleLater()
