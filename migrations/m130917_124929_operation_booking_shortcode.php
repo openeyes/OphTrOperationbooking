dff7523a281d5c1397fd5c7a36bf9ffb3aa15ba2
Mark <m@rkw.me>
2014-01-13 15:53:16 +0000

    fixed use of models

--- migrations/m130917_124929_operation_booking_shortcode.php
+++ migrations/m130917_124929_operation_booking_shortcode.php
@@ -4,8 +4,9 @@ class m130917_124929_operation_booking_shortcode extends CDbMigration
 {
 	public function up()
 	{
-		$event_type = EventType::model()->find('class_name=?',array('OphTrOperationbooking'));
-		$this->insert('patient_shortcode',array('event_type_id'=>$event_type->id,'default_code'=>'obd','code'=>'obd','description'=>'The latest operation booking diagnosis','method'=>'getLatestOperationBookingDiagnosis'));
+		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array("OphTrOperationbooking"))->queryRow();
+
+		$this->insert('patient_shortcode',array('event_type_id'=>$event_type['id'],'default_code'=>'obd','code'=>'obd','description'=>'The latest operation booking diagnosis','method'=>'getLatestOperationBookingDiagnosis'));
 	}
 
 	public function down()
