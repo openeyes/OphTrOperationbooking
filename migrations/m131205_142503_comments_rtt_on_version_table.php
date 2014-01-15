3b8c12e0526c61f1c968d8252f57d62aa59f0a31
Mark <m@rkw.me>
2014-01-15 10:37:43 +0000

    [OE-3914] fixed collation

--- migrations/m131205_142503_comments_rtt_on_version_table.php
+++ migrations/m131205_142503_comments_rtt_on_version_table.php
@@ -4,7 +4,7 @@ class m131205_142503_comments_rtt_on_version_table extends CDbMigration
 {
 	public function up()
 	{
-		$this->addColumn('et_ophtroperationbooking_operation_version', 'comments_rtt', "TEXT NULL COLLATE 'utf8_bin'");
+		$this->addColumn('et_ophtroperationbooking_operation_version', 'comments_rtt', "text NULL");
 	}
 
 	public function down()
