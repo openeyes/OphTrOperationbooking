b59ed738cd9b4fba31dd79d736ad25412204a14c
Jamie Neil <jamie.neil@openeyes.org.uk>
2014-01-24 14:21:56 +0000

    [OEM-121] Change anaesthetic to not null

--- migrations/m140124_095039_default_anaesthetic_id_to_null.php
+++ migrations/m140124_095039_default_anaesthetic_id_to_null.php
@@ -4,7 +4,7 @@ class m140124_095039_default_anaesthetic_id_to_null extends CDbMigration
 {
 	public function up()
 	{
-		$this->alterColumn('et_ophtroperationbooking_operation','anaesthetic_type_id', 'INT(10) UNSIGNED NOT NULL DEFAULT NULL');
+		$this->alterColumn('et_ophtroperationbooking_operation','anaesthetic_type_id', 'INT(10) UNSIGNED NOT NULL');
 	}
 
 	public function down()
