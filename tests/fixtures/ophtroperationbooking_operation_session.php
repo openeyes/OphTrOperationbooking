d5372a19ef0035102bad81f686c762e1d3b31ca6
Mark <m@rkw.me>
2014-01-27 15:04:42 +0000

    tests for the admin and booking controllers

---
+++ tests/fixtures/ophtroperationbooking_operation_session.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * OpenEyes
+ *
+ * (C) Moorfields Eye Hospital NHS Foundation Trust, 2008-2011
+ * (C) OpenEyes Foundation, 2011-2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (c) 2008-2011, Moorfields Eye Hospital NHS Foundation Trust
+ * @copyright Copyright (c) 2011-2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+return array(
+	'ses1' => array(
+		'sequence_id' => 1,
+		'firm_id' => 2,
+		'theatre_id' => '1',
+		'date' => date('Y-m-d'),
+		'start_time' => '08:00:00',
+		'end_time' => '13:00:00',
+		'consultant' => 1,
+		'weekday' => 1,
+		'week_selection' => 31,
+		'paediatric' => 1,
+		'anaesthetist' => 1,
+		'available' => 1,
+		'general_anaesthetic' => 1,
+		'default_admission_time' => '08:00:00',
+		'created_date' => date('Y-m-d 00:00:00'),
+		'last_modified_date' => date('Y-m-d 00:00:00'),
+	),
+	'ses2' => array(
+		'sequence_id' => 1,
+		'firm_id' => 2,
+		'theatre_id' => '1',
+		'date' => date('Y-m-d'),
+		'start_time' => '08:00:00',
+		'end_time' => '13:00:00',
+		'consultant' => 0,
+		'weekday' => 1,
+		'week_selection' => 31,
+		'paediatric' => 1,
+		'anaesthetist' => 1,
+		'available' => 1,
+		'general_anaesthetic' => 1,
+		'default_admission_time' => '08:00:00',
+		'created_date' => date('Y-m-d 00:00:00'),
+		'last_modified_date' => date('Y-m-d 00:00:00'),
+	),
+	'ses3' => array(
+		'sequence_id' => 1,
+		'firm_id' => 2,
+		'theatre_id' => '1',
+		'date' => date('Y-m-d'),
+		'start_time' => '08:00:00',
+		'end_time' => '13:00:00',
+		'consultant' => 1,
+		'weekday' => 1,
+		'week_selection' => 31,
+		'paediatric' => 1,
+		'anaesthetist' => 0,
+		'available' => 1,
+		'general_anaesthetic' => 1,
+		'default_admission_time' => '08:00:00',
+		'created_date' => date('Y-m-d 00:00:00'),
+		'last_modified_date' => date('Y-m-d 00:00:00'),
+	),
+	'ses4' => array(
+		'sequence_id' => 1,
+		'firm_id' => 2,
+		'theatre_id' => '1',
+		'date' => date('Y-m-d'),
+		'start_time' => '08:00:00',
+		'end_time' => '13:00:00',
+		'consultant' => 1,
+		'weekday' => 1,
+		'week_selection' => 31,
+		'paediatric' => 0,
+		'anaesthetist' => 1,
+		'available' => 1,
+		'general_anaesthetic' => 1,
+		'default_admission_time' => '08:00:00',
+		'created_date' => date('Y-m-d 00:00:00'),
+		'last_modified_date' => date('Y-m-d 00:00:00'),
+	),
+	'ses5' => array(
+		'sequence_id' => 1,
+		'firm_id' => 2,
+		'theatre_id' => '1',
+		'date' => date('Y-m-d'),
+		'start_time' => '08:00:00',
+		'end_time' => '13:00:00',
+		'consultant' => 1,
+		'weekday' => 1,
+		'week_selection' => 31,
+		'paediatric' => 1,
+		'anaesthetist' => 1,
+		'available' => 1,
+		'general_anaesthetic' => 0,
+		'default_admission_time' => '08:00:00',
+		'created_date' => date('Y-m-d 00:00:00'),
+		'last_modified_date' => date('Y-m-d 00:00:00'),
+	),
+	'ses6' => array(
+		'sequence_id' => 1,
+		'firm_id' => 2,
+		'theatre_id' => '1',
+		'date' => date('Y-m-d',time()-3600),
+		'start_time' => '08:00:00',
+		'end_time' => '13:00:00',
+		'consultant' => 1,
+		'weekday' => 1,
+		'week_selection' => 31,
+		'paediatric' => 1,
+		'anaesthetist' => 1,
+		'available' => 1,
+		'general_anaesthetic' => 1,
+		'default_admission_time' => '08:00:00',
+		'created_date' => date('Y-m-d 00:00:00'),
+		'last_modified_date' => date('Y-m-d 00:00:00'),
+	)
+);
