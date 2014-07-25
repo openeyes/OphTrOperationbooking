47000c3475b6b8da348d8a66cad58f7f8482169e
Mark Wadham <m@rkw.me>
2014-07-25 13:17:46 +0000

    [ORB-267] fixed crash when scheduling an operation event

--- models/Element_OphTrOperationbooking_Operation.php
+++ models/Element_OphTrOperationbooking_Operation.php
@@ -1031,6 +1031,9 @@ class Element_OphTrOperationbooking_Operation extends BaseEventTypeElement
 			}
 		}
 
+		// If left true this will break horribly
+		$this->auto_update_relations = false;
+
 		if (!$this->save()) {
 			throw new Exception('Unable to update operation data: '.print_r($this->getErrors(),true));
 		}
