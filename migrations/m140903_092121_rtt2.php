719009c2ad60dc653fcf9b46a0b8e10cc0084b25
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-09-03 16:42:30 +0100

    Version table.

--- migrations/m140903_092121_rtt2.php
+++ migrations/m140903_092121_rtt2.php
@@ -5,10 +5,12 @@ class m140903_092121_rtt2 extends OEMigration
 	public function safeUp()
 	{
 		$this->addColumn('et_ophtroperationbooking_operation', 'organising_admission_user_id', 'integer unsigned');
+		$this->addColumn('et_ophtroperationbooking_operation_version', 'organising_admission_user_id', 'integer unsigned');
 	}
 
 	public function safeDown()
 	{
 		$this->dropColumn('et_ophtroperationbooking_operation', 'organising_admission_user_id');
+		$this->dropColumn('et_ophtroperationbooking_operation_version', 'organising_admission_user_id');
 	}
 }
