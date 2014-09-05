2235ff82ed57cff7ac9aa39f018e21ab29c0498d
Ben Spencer <ben.spencer@openeyes.org.uk>
2014-09-05 17:26:24 +0100

    [OEM-219] Display schedule options in booking form.

--- controllers/BookingController.php
+++ controllers/BookingController.php
@@ -190,6 +190,7 @@ class BookingController extends OphTrOperationbookingEventController
 		$this->render('schedule', array(
 			'event' => $this->event,
 			'operation' => $operation,
+			'schedule_options' => $schedule_options,
 			'firm' => $firm,
 			'firmList' => Firm::model()->listWithSpecialties,
 			'date' => $date,
