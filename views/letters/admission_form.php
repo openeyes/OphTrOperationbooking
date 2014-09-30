e8d1390d102f5e2dba12d33f96c7c5853ba6d92d
adamdyer <progen91@gmail.com>
2014-09-30 15:38:04 +0100

    [OE-5098] named consultant on admission form

--- views/letters/admission_form.php
+++ views/letters/admission_form.php
@@ -115,7 +115,14 @@
 
 	<tr>
 		<th>Consultant to be present:</th>
-		<td><?php echo (empty($operation->consultant_required)) ? 'No' : 'Yes'?></td>
+		<td>
+			<?php
+			echo (empty($operation->consultant_required)) ? 'No' : 'Yes';
+			if($operation->consultant_required && $operation->consultant) {
+				echo ', '.$operation->consultant->ReversedFullName;
+			}
+			?>
+		</td>
 		<th>Total theatre time (mins):</th>
 		<td><?php echo CHtml::encode($operation->total_duration)?></td>
 	</tr>
