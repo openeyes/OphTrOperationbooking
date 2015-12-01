62dff6b0af4383e89d83900a75f41d2f278e2048
pgwanz <pgwanz@hotmail.com>
2015-12-01 07:57:37 +0000

    Allow admin to retrospectively book operations into past slots

--- models/OphTrOperationbooking_Operation_Session.php
+++ models/OphTrOperationbooking_Operation_Session.php
@@ -122,6 +122,21 @@ class OphTrOperationbooking_Operation_Session extends BaseActiveRecordVersioned
 		);
 	}
 
+	/**
+	 * @param null
+	 * @return boolean
+	 */
+	private function isAdmin()
+	{
+		$user = Yii::app()->session['user'];
+
+		if($user->role == 'admin role')
+		{
+			return true;
+		}
+        return false;
+    }
+
 	public function getActiveBookingsForWard($ward_id = null)
 	{
 		$criteria = array(
@@ -306,7 +321,7 @@ class OphTrOperationbooking_Operation_Session extends BaseActiveRecordVersioned
 			return false;
 		}
 
-		if ($this->date < date('Y-m-d')) {
+		if (($this->date < date('Y-m-d')) && !($this->isAdmin())) {
 			return false;
 		}
 
@@ -354,7 +369,7 @@ class OphTrOperationbooking_Operation_Session extends BaseActiveRecordVersioned
 			}
 		}
 
-		if ($this->date < date('Y-m-d')) {
+		if (($this->date < date('Y-m-d')) && !($this->isAdmin())) {
 			return "This session is in the past and so cannot be booked into.";
 		}
 
